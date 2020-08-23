b87231e7e6cc2ea2df118f38bbc329793841903d
uga1214 <yoshihisa.ogawa1214@gmail.com>
2020-08-23 21:53:07 +0900

    Rubocopの解析結果をもとに修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,16 +1,13 @@
-# Gemfile.lockを見てcapistranoのバージョンを入れる
 lock '3.14.1'
 
-# 自身のアプリ名、リポジトリ名を記述
 set :application, 'fleamarket_sample_77c'
-set :repo_url,  'git@github.com:ryutaro-hirasawa/fleamarket_sample_77c.git'
+set :repo_url, 'git@github.com:ryutaro-hirasawa/fleamarket_sample_77c.git'
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1'
 
-# chat-spaceで使ったpemを指定
 set :ssh_options, auth_methods: ['publickey'],
                   keys: ['~/.ssh/chat-space.pem']
 
@@ -18,7 +15,7 @@ set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
 
-set :linked_files, %w{ config/master.key }
+set :linked_files, %w[config/master.key]
 
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
@@ -29,10 +26,8 @@ namespace :deploy do
 
   desc 'upload master.key'
   task :upload do
-    on roles(:app) do |host|
-      if test "[ ! -d #{shared_path}/config ]"
-        execute "mkdir -p #{shared_path}/config"
-      end
+    on roles(:app) do |_host|
+      execute "mkdir -p #{shared_path}/config" if test "[ ! -d #{shared_path}/config ]"
       upload!('config/master.key', "#{shared_path}/config/master.key")
     end
   end
