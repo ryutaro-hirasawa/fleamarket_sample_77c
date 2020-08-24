d1d3aeb227d2ca1f1febfdde63b69529cc803c3b
RyuichiroAbe <66513839+RyuichiroAbe@users.noreply.github.com>
2020-08-24 18:53:22 +0900

    必要なモデルを作成し、アソシエーションを設定

---
+++ db/migrate/20200824093524_create_sending_destinations.rb
@@ -0,0 +1,15 @@
+class CreateSendingDestinations < ActiveRecord::Migration[5.2]
+  def change
+    create_table :sending_destinations do |t|
+      t.integer :post_code,        null:false
+      t.integer :prefecture_code,  null:false
+      t.string  :city,             null:false
+      t.string  :house_number,     null:false
+      t.string  :building_name
+      t.string  :phone_number,     unique:true
+
+      t.references	:user,         null:false,foreign_key:true
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_24_001126) do
+ActiveRecord::Schema.define(version: 2020_08_24_093524) do
+
+  create_table "sending_destinations", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "post_code", null: false
+    t.integer "prefecture_code", null: false
+    t.string "city", null: false
+    t.string "house_number", null: false
+    t.string "building_name"
+    t.string "phone_number"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_sending_destinations_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -30,4 +43,5 @@ ActiveRecord::Schema.define(version: 2020_08_24_001126) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "sending_destinations", "users"
 end
