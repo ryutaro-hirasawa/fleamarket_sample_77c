2487a54a5a39aa3a8fb6ddbc47398e115ff49441
ryutaro-hirasawa <doraguna0219@gmail.com>
2020-08-26 11:03:00 +0900

    commit0826

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :email, presence: true, uniqueness: true
-  validates :password, presence: true, length: { minimum: 6 }
-  validates :first_name,:family_name,:first_name_kana,:family_name_kana,:birth_day, presence: true
+
+  # 正規表現を変数に変換
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_KANA_REGEX = /\A[ぁ-ん]+\z/
+
+  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
+  validates :password, presence: true, length: { minimum: 7 }
+  validates :nickname,:first_name,:family_name,:birth_day, presence: true
+  validates :first_name_kana,:family_name_kana, presence: true, format: { with: VALID_KANA_REGEX }
   has_one :sending_destination
 end
