1aa528d490c525977c4c3dea7fb6998a624308a2
RyuichiroAbe <66513839+RyuichiroAbe@users.noreply.github.com>
2020-08-27 21:06:59 +0900

    active_hash追加

--- app/models/sending_destination.rb
+++ app/models/sending_destination.rb
@@ -1,4 +1,6 @@
 class SendingDestination < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
   belongs_to :user, optional: true
   validates :post_code, :prefecture_code, :city, :house_number, :building_name, :phone_number, presence: true
 end
