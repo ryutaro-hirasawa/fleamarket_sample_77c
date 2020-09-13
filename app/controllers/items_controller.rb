05de4c4ab6e8c9e3a7f177d40c33e49437c239ae
RyuichiroAbe <66513839+RyuichiroAbe@users.noreply.github.com>
2020-09-13 14:15:32 +0900

    new_path

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,11 @@
 class ItemsController < ApplicationController
   def index; end
 
+  def new
+
+  end
+
+
   def show
     
   end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,6 @@ Rails.application.routes.draw do
   end
   root to: "items#index"
   resources :users, :only => [:show]
-  resources :items, :only => [:show]
+  resources :items, :only => [:show, :new]
 
 end
