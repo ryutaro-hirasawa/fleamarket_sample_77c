b3985d449d97ae5c3f518d8f30df8e9b50d1eabf
kotaro-imao <kotaroima4@gmail.com>
2020-10-03 13:55:48 +0900

    RSpec test 1 imao 1003

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,84 @@
 require 'rails_helper'
 
-RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Item do
+
+  # item = Item.new(name: "商品", introduction: "ホワイト", category_id: "1", item_condition_id: "1", postage_payer_id: "1", prefecture_code_id: "1", preparation_day_id: "1", price: "3000",)
+  item = FactoryBot.build(:item)
+
+  describe '#create' do
+
+    # it  "name と introduction, category_id, item_condition_id, postage_payer_id, prefecture_code_id, preparation_day_id, price があれば登録できること" do
+    # item = build(:item)
+    #   expect(item).to be_valid
+    # end
+    
+
+
+    # it "必須項目が全て存在すれば登録できること" do
+    #   item = build(:item)
+    #   expect(item).to be_valid
+    # end
+
+    # it "brandが存在すれば登録できること" do
+    #   item = build(:item, brand: "nike")
+    #   expect(item).to be_valid
+    # end
+    
+    it "nameがない場合は登録できないこと" do
+      item = build(:item, name: nil)
+      item.valid?
+      expect(item.errors[:name]).to include("can't be blank")
+    end
+
+
+    it "introductionがない場合は登録できないこと" do
+      item = build(:item, introduction: nil)
+      item.valid?
+      expect(item.errors[:introduction]).to include("can't be blank")
+    end
+
+    it "category_id がない場合は登録できないこと" do
+      item = build(:item, category_id: nil)
+      item.valid?
+      expect(item.errors[:category_id ]).to include("can't be blank")
+    end
+
+    it "item_condition_id がない場合は登録できないこと" do
+      item = build(:item, item_condition_id: nil)
+      item.valid?
+      expect(item.errors[:item_condition_id ]).to include("can't be blank")
+    end
+
+    it "postage_payer_id がない場合は登録できないこと" do
+      item = build(:item, postage_payer_id: nil)
+      item.valid?
+      expect(item.errors[:postage_payer_id ]).to include("can't be blank")
+    end
+
+    it "prefecture_code_id がない場合は登録できないこと" do
+      item = build(:item, prefecture_code_id: nil)
+      item.valid?
+      expect(item.errors[:prefecture_code_id ]).to include("can't be blank")
+    end
+
+    it "preparation_day_id がない場合は登録できないこと" do
+      item = build(:item, preparation_day_id: nil)
+      item.valid?
+      expect(item.errors[:preparation_day_id ]).to include("can't be blank")
+    end
+
+    it "price がない場合は登録できないこと" do
+      item = build(:item, price: nil)
+      item.valid?
+      expect(item.errors[:price ]).to include("can't be blank")
+    end
+
+
+  end
 end
+
+# RSpec.describe Item, type: :model do
+#   pending "add some examples to (or delete) #{__FILE__}"
+
+# end
+
