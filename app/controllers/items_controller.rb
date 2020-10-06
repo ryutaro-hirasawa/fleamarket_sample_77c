86d9dff37bc7bbc2f4c6bd846734eb39444a1a1a
RyuichiroAbe <66513839+RyuichiroAbe@users.noreply.github.com>
2020-10-06 18:31:52 +0900

    edit_1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,8 @@
 class ItemsController < ApplicationController
 
+  before_action :category_parent_array, only: [:new, :create, :edit]
   before_action :set_item, except: [:index, :new, :create, :category_children, :category_grandchildren]
-
+  before_action :show_all_instance, only: [:show, :edit, :destroy]
 
   def index
     @item = Item.includes(:images).order('created_at DESC')
@@ -38,11 +39,53 @@ class ItemsController < ApplicationController
     end
   end
 
-  def update
-    if @item.update(item_params)
-      redirect_to root_path
+  def edit
+    # ▼ ①ここで該当商品の子・孫カテゴリーを変数へ代入
+    grandchild = @item.category
+    child = grandchild.parent
+    if @category_id == 46 or @category_id == 74 or @category_id == 134 or @category_id == 142 or @category_id == 147 or @category_id == 150 or @category_id == 158
     else
+     # ② ▼ 親カテゴリーのnameとidを配列代入
+     @parent_array = []
+     @parent_array << @item.category.parent.parent.name
+     @parent_array << @item.category.parent.parent.id
+    end
+     # ③ ▼ 子カテゴリーを全てインスタンス変数へ代入
+     @category_children_array = Category.where(ancestry: child.ancestry)
+     # ④ ▼ 子カテゴリーのnameとidを配列代入
+     @child_array = []
+     @child_array << child.name # ⑤で生成した変数を元にname・idを取得
+     @child_array << child.id
+     # ⑤ ▼ 孫カテゴリーを全てインスタンス変数へ代入
+     @category_grandchildren_array = Category.where(ancestry: grandchild.ancestry) 
+     # ⑥ ▼ 孫カテゴリーのnameとidを配列代入
+     @grandchild_array = []
+     @grandchild_array << grandchild.name # ⑤で生成した変数を元にname・idを取得
+     @grandchild_array << grandchild.id
+  end
+
+  def update
+    if item_params[:images_attributes].nil?
+      flash.now[:alert] = '更新できませんでした 【画像を１枚以上入れてください】'
       render :edit
+    else
+      exit_ids = []
+      item_params[:images_attributes].each do |a,b|
+        exit_ids << item_params[:images_attributes].dig(:"#{a}",:id).to_i
+    end
+
+      ids = Image.where(item_id: params[:id]).map{|image| image.id }
+      
+      delete__db = ids - exit_ids
+      Image.where(id:delete__db).destroy_all
+      
+      @item.touch
+      if @item.update(item_params)
+        redirect_to  root_path
+      else
+        flash.now[:alert] = '更新できませんでした'
+        render :edit
+      end
     end
   end
 
@@ -64,7 +107,20 @@ class ItemsController < ApplicationController
   end
 
   def set_item
-    @item = Item.find(params[:id])
+    @item = Item.find(4)
+  end
+
+  def category_parent_array
+    @category_parent_array = Category.where(ancestry: nil)        # ⑧ 親カテゴリーを全てインスタンス変数へ代入
+  end
+
+  def show_all_instance
+    @images = Image.where(item_id: params[:id])                   # ⑨ 該当商品の画像をインスタンス変数へ代入
+    @images_first = Image.where(item_id: params[:id]).first
+    @category_id = @item.category_id                              # ⑩ 該当商品のレコードからカテゴリーidを取得し、インスタンス変数へ代入（この際に取得するidは孫カテゴリーidです。）
+    @category_parent = Category.find(@category_id).parent.parent                    
+    @category_child = Category.find(@category_id).parent
+    @category_grandchild = Category.find(@category_id)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,12 @@ Rails.application.routes.draw do
     get 'sending_destinations', to: 'users/registrations#new_sending_destinations'
     post 'sending_destinations', to: 'users/registrations#create_sending_destinations'
   end
-  root to: "items#index"
+  root to: "items#edit"
   resource :items, only:[:show,:new, :create, :edit, :update, :destroy] do
     collection do
       get 'category_children', defaults: { format: 'json' }
       get 'category_grandchildren', defaults: { format: 'json' }
+      get 'update_done'
     end
   end
   resources :users, :only => [:show]
