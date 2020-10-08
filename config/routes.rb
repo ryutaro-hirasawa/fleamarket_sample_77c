e876a8ff337ba6d27d046f79f964f1325cf80c52
RyuichiroAbe <66513839+RyuichiroAbe@users.noreply.github.com>
2020-10-08 16:11:01 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
     get 'sending_destinations', to: 'users/registrations#new_sending_destinations'
     post 'sending_destinations', to: 'users/registrations#create_sending_destinations'
   end
-  root to: "items#edit"
+  root to: "items#index"
   resource :items, only:[:show,:new, :create, :edit, :update, :destroy] do
     collection do
       get 'category_children', defaults: { format: 'json' }
