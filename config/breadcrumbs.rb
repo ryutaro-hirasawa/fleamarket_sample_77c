0952e33517dcbae146d7226f7c7b8cbda1f2c71f
uga1214 <yoshihisa.ogawa1214@gmail.com>
2020-10-18 14:09:08 +0900

    Add breadcrumbs to Card/Purchases/Mypage

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -1,6 +1,53 @@
+
 crumb :root do
-  link "Home", root_path
+  link "トップページ", root_path
 end
+
+crumb :mypage do
+  link "マイページ", user_path(current_user)
+  parent :root
+end
+
+crumb :card_new do
+  link "クレジットカード登録", new_card_path
+  parent :mypage
+end
+
+crumb :card_create do
+  link "クレジットカード登録完了", cards_path
+  parent :mypage
+end
+
+crumb :card_show do
+  link "登録クレジットカード", card_path(current_user)
+  parent :mypage
+end
+
+crumb :card_destroy do
+  link "登録クレジットカード削除完了", card_path(current_user)
+  parent :mypage
+end
+
+crumb :items_new do
+  link "出品ページ", new_item_path
+  parent :root
+end
+
+crumb :items_show do
+  link "商品の詳細", item_path(items_url)
+  parent :root
+end
+
+crumb :purchases_buy do
+  link "購入内容の確認", buy_item_purchases_path
+  parent :items_show
+end
+
+crumb :purchases_pay do
+  link "購入完了", pay_item_purchases_path
+  parent :purchases_buy
+end
+
 
 # crumb :projects do
 #   link "Projects", projects_path
